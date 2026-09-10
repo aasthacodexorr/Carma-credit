890655e769460dafbc8c0600b7aa9614dccf2ed1
AasthaCodexorr <aastha.codexorr@gmail.com>
2026-09-10 10:49:44 +0530

    UI of about us page

--- components/pages/AboutUsPage.tsx
+++ components/pages/AboutUsPage.tsx
@@ -42,7 +42,7 @@ export default function Home() {
           </div>
 
           {/* LEFT CONTENT: Vertically centered on solid light background with no overlap */}
-          <div className="relative z-20 w-full max-w-[1440px] mx-auto px-6 sm:px-10 lg:pl-16 xl:pl-24 py-14 sm:py-16 lg:py-20">
+          <div className="relative z-20 w-full max-w-[1480px] mx-auto px-6 sm:px-10 lg:pl-16 xl:pl-24 py-14 sm:py-16 lg:py-20">
             <div className="max-w-[480px] xl:max-w-[510px] flex flex-col items-start justify-center">
               <span className="text-xs sm:text-[13px] font-bold tracking-[0.2em] text-[#ff385c] uppercase mb-4">
                 About Carma Credit
