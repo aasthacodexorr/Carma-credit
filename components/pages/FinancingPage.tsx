a84ac6381b5ebad07ce957eca912d9ac8095c3c6
AasthaCodexorr <aastha.codexorr@gmail.com>
2026-09-14 14:34:02 +0530

    updated script origin for iframes

--- components/pages/FinancingPage.tsx
+++ components/pages/FinancingPage.tsx
@@ -9,7 +9,7 @@ import { PageShell } from "@/components/layout";
 import { getConstants } from "@/constants";
 import { useAppConfig } from "@/app/providers";
 
-const MIN_HEIGHT = 1952;
+const MIN_HEIGHT = 1352;
 const FALLBACK_HEIGHT = 1272;
 
 const Finance = () => {
