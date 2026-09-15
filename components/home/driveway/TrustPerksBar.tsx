9971b0e5a9711306afb7304775958328ffebfd2b
AasthaCodexorr <aastha.codexorr@gmail.com>
2026-09-15 14:12:45 +0530

    terms images should be in pink color and fix layout

--- components/home/driveway/TrustPerksBar.tsx
+++ components/home/driveway/TrustPerksBar.tsx
@@ -26,9 +26,17 @@ const TrustPerksBar = () => {
       <div className="mx-auto max-w-[1480px] px-6 sm:px-12 xl:px-16">
         <div className="grid grid-cols-2 gap-6 lg:grid-cols-4 lg:divide-x lg:divide-slate-200">
           {perks.map((perk, index) => (
-            <div key={index} className="flex items-center space-x-3 lg:px-6 first:lg:pl-0 last:lg:pr-0">
-              <div className="flex h-14 w-14 shrink-0 p-1 items-center justify-center rounded-xl bg-slate-100">
-                <img src={perk.icon}/>
+            <div key={index} className="flex flex-col items-center space-x-3 lg:px-6 first:lg:pl-0 last:lg:pr-0">
+              <div className="flex  h-14 w-14 shrink-0 p-1 items-center justify-center rounded-xl">
+                <img
+                  src={perk.icon}
+                  alt=""
+                  className="h-full w-full object-contain"
+                  style={{
+                    filter:
+                      "brightness(0) saturate(100%) invert(34%) sepia(95%) saturate(3637%) hue-rotate(328deg) brightness(101%) contrast(101%)",
+                  }}
+                />
               </div>
               <div>
                 <h4 className="text-base font-bold text-[#131b4d] sm:text-base leading-tight">
